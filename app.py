017d515b61764d60b072f81752cb9dba1aab7ac4
Alex Lopes <aclopes@gmail.com>
2019-05-21 15:11:33 -0400

    Initial fixies

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, redirect, url_for, session, g, flash, render_template
+from flask import Flask, request, redirect, url_for, session, flash, render_template
 # from flask_oauth import OAuth
-import requests
 import urllib
 from werkzeug.exceptions import BadRequest
 from QBOService import create_customer, get_companyInfo
 from utils import excel, context, OAuth2Helper
 import config
+
 
 # configuration
 SECRET_KEY = 'dev key'
@@ -15,6 +15,10 @@ DEBUG = True
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
+
+
+
+
 
 @app.route('/')
 def index():
@@ -27,18 +31,22 @@ def index():
         title="QB Customer Leads",
     )
 
+
+
+
+
 @app.route('/', methods=['POST'])
 def update_table():
     """Update Excel file after customer is added in QBO"""
     customer_id = request.form['id']
-  
+
     request_context = context.RequestContext(session['realm_id'], session['access_token'], session['refresh_token'])
 
     for customer in customer_list:
         if customer['Id'] == customer_id:
             # Create customer object
             response = create_customer(customer, request_context)
-            
+
             # If customer added successfully, remove them from html and excel file
             if (response.status_code == 200):
                 font_color = 'green'
@@ -55,11 +63,15 @@ def update_table():
                 flash('Something went wrong: ' + response.text)
     return redirect(url_for('index'))
 
+
+
+
+
 @app.route('/company-info')
 def company_info():
     """Gets CompanyInfo of the connected QBO account"""
     request_context = context.RequestContext(session['realm_id'], session['access_token'], session['refresh_token'])
-    
+
     response = get_companyInfo(request_context)
     if (response.status_code == 200):
         return render_template(
@@ -75,20 +87,28 @@ def company_info():
             company_info=response.text,
             title='QB Customer Leads',
         )
-    
+
+
+
+
+
 @app.route('/auth')
 def auth():
     """Initiates the Authorization flow after getting the right config value"""
     params = {
-        'scope': 'com.intuit.quickbooks.accounting', 
+        'scope': 'com.intuit.quickbooks.accounting',
         'redirect_uri': config.REDIRECT_URI,
-        'response_type': 'code', 
+        'response_type': 'code',
         'client_id': config.CLIENT_ID,
         'state': csrf_token()
     }
     url = OAuth2Helper.get_discovery_doc()['authorization_endpoint'] + '?' + urllib.parse.urlencode(params)
     return redirect(url)
-   
+
+
+
+
+
 @app.route('/reset-session')
 def reset_session():
     """Resets session"""
@@ -96,10 +116,14 @@ def reset_session():
     session['is_authorized'] = False
     return redirect(request.referrer or url_for('index'))
 
+
+
+
+
 @app.route('/callback')
 def callback():
     """Handles callback only for OAuth2"""
-    #session['realmid'] = str(request.args.get('realmId'))
+    # session['realmid'] = str(request.args.get('realmId'))
     state = str(request.args.get('state'))
     error = str(request.args.get('error'))
     if error == 'access_denied':
@@ -108,21 +132,25 @@ def callback():
         return BadRequest()
     elif state != csrf_token():  # validate against CSRF attacks
         return BadRequest('unauthorized')
-    
+
     auth_code = str(request.args.get('code'))
     if auth_code is None:
         return BadRequest()
-    
+
     bearer = OAuth2Helper.get_bearer_token(auth_code)
     realmId = str(request.args.get('realmId'))
 
     # update session here
-    session['is_authorized'] = True 
+    session['is_authorized'] = True
     session['realm_id'] = realmId
     session['access_token'] = bearer['access_token']
     session['refresh_token'] = bearer['refresh_token']
 
     return redirect(url_for('index'))
+
+
+
+
 
 def csrf_token():
     token = session.get('csrfToken', None)
@@ -130,6 +158,10 @@ def csrf_token():
         token = OAuth2Helper.secret_key()
         session['csrfToken'] = token
     return token
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
